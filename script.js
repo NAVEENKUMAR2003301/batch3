9df6a5a4de0b488970d474df7045c5fd4b4de358
NAVEENKUMAR2003301 <livnaveen@gmail.com>
2025-06-27 16:37:49 +0530

    dom

--- script.js
+++ script.js
@@ -708,67 +708,61 @@
 // function - for of loop
 
 
-let arr = [10,20,30,40]
-for(let i=0;i<arr.length;i++){
-    console.log(arr[i]);   
-}
+// let arr = [10,20,30,40]
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);   
+// }
 
-let str = "javascript"
-for(let i=0;i<str.length;i++){
-    console.log(str[i])
-}
+// let str = "javascript"
+// for(let i=0;i<str.length;i++){
+//     console.log(str[i])
+// }
 
-let arr1 = [10,100,200,300]
+// let arr1 = [10,100,200,300]
 
-for(let array of arr1){
-    console.log(array);   
-}
+// for(let array of arr1){
+//     console.log(array);   
+// }
 
-let str1 = "web development"
-for(let string of str1){
-    console.log(string);
+// let str1 = "web development"
+// for(let string of str1){
+//     console.log(string);
     
-}
+// }
 
-function*genFun(){
-    yield "one"
-    yield "two"
-    yield "three"
-}
+// for (let arrayy in arr1){
+//     console.log(arrayy)
+// }
 
-let iteration = genFun();
-for(let value of iteration){
-    console.log(value)
-}
+// function*genFun(){
+//     yield "one"
+//     yield "two"
+//     yield "three"
+// }
+
+// let iteration = genFun();
+// for(let func of iteration){
+//     console.log(func)
+// }
 
 // for in loop 
 
-let person1 = {
-    uName : "kumerasan",
-    hobbies : ["cricket","volley ball","foot Ball"],
-    familyDetials : {
-        totalMem : 5,
-        siblings : ["a","b"]
-    },
-    walk(){
-        console.log("I am Going to Home")
-    }
-}
+// let person1 = {
+//     uName : "kumerasan",
+//     hobbies : ["cricket","volley ball","foot Ball"],
+//     familyDetials : {
+//         totalMem : 5,
+//         siblings : ["a","b"]
+//     },
+//     walk(){
+//         console.log("I am Going to Home")
+//     }
+// }
 
-for(let key in person1){
-    console.log(key+" "+person1[key]);
-    person1.walk()
-    
-}
-
-for(let you in arr){
-    console.log(arr[you]); 
-}
-
-for(let get in str1){
-    console.log(str1[get]);
-    
-}
+// for (let obj in person1){
+//     console.log(person1[obj])
+//     console.log(person1.walk()); 
+// }
 
 
 // es6 edition 
@@ -792,34 +786,536 @@ for(let get in str1){
 
 // array 
 
-let hobbies = ["cricket","vollyball","Art"]
-let hobbies1 = ["reading","coding"]
-let hobbies2 = ["writing","teachinng"]
+// let hobbies = ["cricket","vollyball","Art"]
+// let hobbies1 = ["reading","coding"]
+// let hobbies2 = ["writing","teachinng"]
 
 
-let newArr = [...hobbies,...hobbies1,...hobbies2]
-let newArr1 = [...newArr,"video Editing","singing"]
+// let newArr = [...hobbies,...hobbies1,...hobbies2]
+// let newArr1 = [...newArr,"video Editing","singing"]
 
-console.log(newArr1);
+// console.log(newArr1);
 
 // object 
 
-let employeeDetials = {
-    empId : "id213",
-    empName : "kumerasan",
-    employeeRole : "frontend developer"
-}
+// let employeeDetials = {
+//     empId : "id213",
+//     empName : "kumerasan",
+//     employeeRole : "frontend developer"
+// }
 
-let team2 = {...employeeDetials,empId:"id234"}
-console.log(team2);
+// let team2 = {...employeeDetials,empName:"mathika"}
+// let team3 = {...employeeDetials,empName:"kumerasan"}
+// console.log(team3);
 
 // rest operator 
 
-function rest(a,b,...arr){
-    console.log(arr);
-    console.log(a,b,arr);
+// function rest(a,b,...arr){
+//     console.log(arr);
+//     console.log(arr+" ."+a+" "+b);
     
+    
+// }
+
+// rest(1,2,3,4,5)
+
+
+// de-structuring
+
+
+// let arr = [10,20,30,40]
+
+// let newVal = arr[0]
+
+// console.log(newVal)
+
+
+// let [a,,,d] = arr
+
+// console.log(a,d)
+
+// let arr1 = [1,2,3,4,5,6,8,9,5,5,54,3,]
+
+// let[a1,a2,a3,...a4] = arr1
+
+// console.log(a1,a2,a3,a4);
+
+
+// let arr2 = [1,2,3,4,[10,20,30,[400,500]]];
+
+// let[a1,a2,a3,a4,[a5,a6,a7,[a8,a9]]] = arr2
+
+// console.log(a1,a2,a3,a4,a5,a6,a7,a8,a9)
+
+
+// array manipulation 
+
+// 1.push - last add,
+// 2.pop - last remove,
+// 3.unshift - first add,
+// 4.shift - first remove,
+// 5.splice - start index , remove , adding
+
+// let arr =[100,200,300,400]
+// arr.splice(1,1,10,20,40)
+// console.log(arr)
+
+
+// array merge method
+
+// 1.concat
+// 2.slice
+// 3.flat 
+// 4.fill
+
+
+// 1.concat 
+// let arr = [1,2,3,4]
+
+
+// let newArr = arr.concat(5,6)
+
+// console.log(newArr);
+// 2.slice 
+// let arr1= [10,20,30,40,60,70,70,50]
+
+// let sliceVal = arr1.slice(3,7)
+
+// console.log(sliceVal)
+
+// 3.flat /
+// let arr2 = [1,[2,3,[4,[5,6]]]]
+
+// let flatVal = arr2.flat(Infinity)
+
+// console.log(flatVal)
+
+// 4. fill 
+
+// let arr4 = [10,20,30,40]
+
+// arr4.fill(101,0,2)
+
+// console.log(arr4);
+
+
+
+// acending order -- sort
+//  --- reverse 
+
+// let arr5 = [1,2,3,7,9,4,6]
+
+// arr5.reverse()
+
+// console.log(arr5)
+
+
+// includes
+
+// let arr6 = [10,20,30,40]
+
+
+// console.log(arr6.includes(90))
+
+
+// function findVowels(alph){
+//     let vowels = "aeiouAEIOU"
+//     let count = 0 
+//     for(let i = 0;i<alph.length;i++){
+//         if(vowels.includes(alph[i])){
+//             count++
+//         }
+//     }
+//     return count   
+// }
+
+// let a = findVowels("bentenition")
+// console.log(a)
+
+
+
+// convert to string 
+
+// let arr7 = [1,2,3,4,5]
+
+// let joinVal = arr7.join(0)
+
+// console.log(joinVal)
+
+// let arr8 = [10,20,30,40]
+
+// let strVal = arr8.toString()
+
+// console.log(strVal)
+
+
+// find index 
+
+
+// 1.indexOf 
+// 2.lastIndexOf 
+
+// let arr9 = [10,20,30,40,50,20]
+
+// let findIn = arr9.indexOf(20,2)
+
+// console.log(findIn)
+
+// let arr10 = [10,20,30,20,10]
+
+// let lastIn = arr10.lastIndexOf(20,2)
+
+// console.log(lastIn)
+
+
+
+
+
+
+
+
+
+
+
+// higher order method 
+
+// for use in UI display 
+
+// forEach 
+// Map
+
+// forEach 
+
+
+// fruit.forEach(printFruit)
+
+// function printFruit(cEle,index,totalArray){
+    //   console.log(totalArray);
+    // }
+    
+    
+    
+    // map 
+    // fruit.map(function (cEle,index,total){
+    //     console.log(total);
+        
+    // })
+        
+    // difference forEach vs map 
+    
+    // for each 
+// let fruit = ["apple","orange","banana","guava"]
+// let newArr = fruit.forEach((cEle)=>{
+//     return cEle
+// })
+
+// console.log(newArr);
+
+// Map 
+
+// let newArr1 = fruit.map((cEle)=>{
+// return cEle
+// })
+
+// console.log(newArr1);
+
+
+// map example :-
+
+// let fruit1 = ["apple","guava","banana","cherry"]
+
+// let shop = fruit1.map(cEle=>cEle.toUpperCase()).sort()
+
+// console.log(shop);
+
+
+
+
+
+
+
+// higher order array method 
+
+// 1.filter
+// 2. find
+
+
+// 1. filter - multiple value stored 
+
+// let employees = [
+//     {empName : "mathika", salary : 100000},
+//     {empName : "kumerasan", salary : 150000},
+//     {empName : "naveen", salary : 170000}, 
+// ]
+
+// let salaryDate = employees.filter(cEle => cEle.salary>=100000
+//  )
+// console.log(salaryDate);
+
+
+// find 
+
+// let finding = employees.find((val)=>{
+//     return val.salary >= 100000
+// })
+
+// console.log(finding);
+
+
+
+
+// higher order function 
+
+// 1.sort
+// 2.some
+// 3.every
+
+// 1.sort 
+
+// let array = [10,5,100,30,6,2,true,false,"string",{}]
+
+// let sorting = array.sort((a,b)=>{
+//    return a-b
+// })
+
+// console.log(sorting);
+
+
+// 2.some 
+
+// let array1 = [2,2,2,2,1]
+
+// let value = array1.some((ele,index,totalArray)=>{
+//   return ele % 2 == 0
+// })
+
+// console.log(value);
+
+// 2.every 
+
+// let value1 = array1.every((ele,index,totalArray)=>{
+//     return ele % 2 == 0
+//   })
+
+//   console.log(value1);
+  
+
+
+
+// reduce 
+
+
+
+// let arr = [10,2,3,4,5,676]   
+
+
+// let total = arr.reduce((acc,cele,index,totalArray)=>{
+//     return acc + cele 
+// },0)
+
+// console.log(total);
+
+
+// string 
+
+// let str = " java-script "
+
+// console.log(str.charAt(9));
+// console.log(str.charCodeAt(5));
+// console.log(str.includes("j"));
+// console.log(str.indexOf("a",2));
+// console.log(str.lastIndexOf("a",10));
+// console.log(str.repeat(1));
+// console.log(str.replace("javascraiapt","python"));
+// console.log(str.split("a"));
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.trim());
+// console.log(str.trimEnd());
+
+
+// console.log(Math.abs(-50));
+// console.log(Math.sign(0));
+// console.log(Math.sqrt(25));
+// console.log(Math.cbrt(8));
+// console.log(Math.pow(3,3));
+
+// let min = [1,4,67,8]
+// console.log(Math.max(...min));
+
+// let new1 = Math.random()
+// console.log(new1);
+
+
+
+// let randomNum = Math.random()*10
+
+// let num = randomNum.toFixed(2)
+
+// console.log(Math.ceil(2.1));
+// console.log(Math.floor(2.9));
+// console.log(Math.round(3.3));
+
+
+
+
+
+
+// console.log(Math.trunc(9.3675578));
+
+
+
+
+
+// date object 
+
+// let date = new Date
+
+
+// console.log(date);
+// console.log(date.getFullYear());
+// console.log(date.getMonth());
+// console.log(date.getDate());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
+
+
+// date.setFullYear(2022,5,24)
+// console.log(date);
+// date.setHours(2,2)
+// console.log(date);
+
+// console.log(date.toDateString());
+// console.log(date.toLocaleString());
+// console.log(date.toLocaleTimeString());
+
+
+// var a = document.firstElementChild.lastElementChild.firstElementChild.firstElementChild
+// console.log(a.textContent);
+
+// var b = document.querySelector(".second")
+// console.log(b.textContent);
+
+// var c = document.querySelector("#theird")
+// console.log(c.textContent);
+
+// var d = document.getElementById("first")
+
+// console.log(d.textContent);
+
+
+// var a = document.getElementById("one")
+
+// a.textContent="hi world"
+
+// var b = document.querySelector(".second")
+
+// b.textContent="JAVASCRIPT"
+
+
+// var one = document.getElementById("one")
+
+// function change(){
+//     one.textContent="hello country"
+// }
+
+
+// var box1 = document.getElementById("num1")
+// var box2 = document.getElementById("num2")
+// var result = document.getElementById("result")
+
+// function resultFunction(){
+//      var box1Value = Number(box1.value)
+//      var box2Value = Number(box2.value)
+
+//      var total = box1Value * box2Value
+
+//     result.textContent = total
+// }
+
+
+// var guessNumber = document.getElementById("guessNumber")
+// var result = document.getElementById("result")
+// var life = document.getElementById("life")
+// var randomGuess  = document.getElementById("randomGuess")
+// var totalLife = 10
+
+
+// alert("welcome to number show")
+// function check(){
+//     var randomNumber = Math.floor(Math.random()*10)+1
+//     var enterNumber = Number(guessNumber.value)
+//     randomGuess.textContent = "randomGuess :-" + randomNumber
+//     if(randomNumber===enterNumber){
+//         result.textContent = "right"
+//         alert("you win")
+//     }else{
+//         result.textContent = "wrong"
+//         totalLife = totalLife - 1
+//         life.textContent = "life = "+ totalLife
+
+//         if(totalLife == 0){
+//             location.reload()
+//         }
+//     }
+    
+
+// }
+
+
+// var box = document.querySelector(".box")
+
+// function color(){
+//     // box.style.backgroundColor = "red"
+//     // box.style.color = "white"
+
+//     box.setAttribute("class","width")
+// }
+
+// var one = document.getElementById("one")
+// var typing = document.getElementById("typing")
+
+// function press(){
+//     typing.textContent = one.value
+// }
+
+// var button = document.getElementById("button")
+
+// function color(){
+//     button.style.backgroundColor = "red"
+// }
+
+// var key = document.getElementById("key")
+
+// function update(event){
+//   key.textContent=event.target.textContent
+   
+// }
+
+
+// function del(event){
+//     event.target.remove()
+// }
+
+// var result1 = document.getElementById("result1")
+
+// function add(){
+//     var list = document.createElement("h1")
+//     list.textContent = "hello"
+//     result1.append(list)
+// }
+
+var input = document.getElementById("three")
+var valuee = document.getElementById("valuee")
+
+function add(){
+    console.log(valuee.textContent);
+   
+
+    var list = document.createElement("h1")
+    list.textContent = input.value
+
+    valuee.append(list)
     
 }
-
-rest(1,2,3,4,5)
